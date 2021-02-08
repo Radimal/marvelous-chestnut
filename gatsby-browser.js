6167c11440c10784b1a1ad3ac8a7f52fd004ffa5
Oliver Plunkett <otplunkett@gmail.com>
2021-02-08 11:29:24 -0500

    exports. > export

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,24 +10,38 @@
 // window.onGatsbyInitialClientRender() has run
 let initialized = false;
 
-exports.onInitialClientRender = () => {
-    initialized = true;
-    if ('onGatsbyInitialClientRender' in window && typeof window.onGatsbyInitialClientRender === 'function') {
-        window.onGatsbyInitialClientRender();
-    }
-    if ('onGatsbyRouteUpdate' in window && typeof window.onGatsbyRouteUpdate === 'function') {
-        window.onGatsbyRouteUpdate();
-    }
+export const onInitialClientRender = () => {
+  initialized = true;
+  if (
+    "onGatsbyInitialClientRender" in window &&
+    typeof window.onGatsbyInitialClientRender === "function"
+  ) {
+    window.onGatsbyInitialClientRender();
+  }
+  if (
+    "onGatsbyRouteUpdate" in window &&
+    typeof window.onGatsbyRouteUpdate === "function"
+  ) {
+    window.onGatsbyRouteUpdate();
+  }
 };
 
-exports.onRouteUpdate = () => {
-    if (initialized && 'onGatsbyRouteUpdate' in window && typeof window.onGatsbyRouteUpdate === 'function') {
-        window.onGatsbyRouteUpdate();
-    }
+export const onRouteUpdate = () => {
+  if (
+    initialized &&
+    "onGatsbyRouteUpdate" in window &&
+    typeof window.onGatsbyRouteUpdate === "function"
+  ) {
+    window.onGatsbyRouteUpdate();
+  }
 };
 
-exports.onPreRouteUpdate = () => {
-    if (initialized && 'onGatsbyPreRouteUpdate' in window && typeof window.onGatsbyPreRouteUpdate === 'function') {
-        window.onGatsbyPreRouteUpdate();
-    }
+export const onPreRouteUpdate = () => {
+  if (
+    initialized &&
+    "onGatsbyPreRouteUpdate" in window &&
+    typeof window.onGatsbyPreRouteUpdate === "function"
+  ) {
+    window.onGatsbyPreRouteUpdate();
+  }
 };
