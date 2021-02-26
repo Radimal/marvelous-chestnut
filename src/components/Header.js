e3e67fb0708ef847433814c92343b4cd5ca3c457
Stackbit Code Editor <support@stackbit.com>
2021-02-26 18:14:38 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -142,7 +142,7 @@ export function Header(props) {
                           className="button"
                           onClick={() => loginWithRedirect()}
                         >
-                          Create Radimal Account
+                          Start Free Trial
                         </button>
                       )}
                     </ul>
