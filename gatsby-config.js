dd0812fa9410991f3292b7b5058f8850e86a8695
Oliver Plunkett <otplunkett@gmail.com>
2021-03-05 17:15:29 -0500

    add back GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,33 +29,33 @@ module.exports = {
         pageContextProperty: `menus`,
       },
     },
-    // {
-    //   resolve: "gatsby-plugin-google-tagmanager",
-    //   options: {
-    //     id: "GTM-MMM2ZDD",
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-MMM2ZDD",
 
-    //     // Include GTM in development.
-    //     //
-    //     // Defaults to false meaning GTM will only be loaded in production.
-    //     includeInDevelopment: false,
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
 
-    //     // datalayer to be set before GTM is loaded
-    //     // should be an object or a function that is executed in the browser
-    //     //
-    //     // Defaults to null
-    //     defaultDataLayer: { platform: "radimal" },
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "radimal" },
 
-    //     // Specify optional GTM environment details.
-    //     // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-    //     // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-    //     // dataLayerName: 'YOUR_DATA_LAYER_NAME',
+        // Specify optional GTM environment details.
+        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
+        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
+        // dataLayerName: 'YOUR_DATA_LAYER_NAME',
 
-    //     // Name of the event that is triggered
-    //     // on every Gatsby route change.
-    //     //
-    //     // Defaults to gatsby-route-change
-    //     // routeChangeEventName: 'YOUR_ROUTE_CHANGE_EVENT_NAME',
-    //   },
-    // },
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: 'YOUR_ROUTE_CHANGE_EVENT_NAME',
+      },
+    },
   ],
 };
