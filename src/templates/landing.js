df6e30bb5f21e906f5a4f96484ae63b0a9c099d7
Oliver Plunkett <otplunkett@gmail.com>
2021-03-05 16:01:09 -0500

    add calendar

--- src/templates/landing.js
+++ src/templates/landing.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import _ from 'lodash';
-import {graphql} from 'gatsby';
+import React from "react";
+import _ from "lodash";
+import { graphql } from "gatsby";
 
-import components, {Layout} from '../components/index';
-import ScriptInjector from "../services/ScriptInjector";
+import components, { Layout } from "../components/index";
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
 export const query = graphql`
   query($url: String) {
-    sitePage(path: {eq: $url}) {
+    sitePage(path: { eq: $url }) {
       id
     }
   }
 `;
 
 export default class Landing extends React.Component {
-    render() {
-
-        const scriptInjector = new ScriptInjector()
-        console.log(scriptInjector.getScripts())
-
-        return (
-            <Layout {...this.props}>
-            {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
-                let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
-                let Component = components[component];
-                return (
-                  <Component key={section_idx} {...this.props} section={section} site={this.props.pageContext.site} />
-                )
-            })}
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout {...this.props}>
+        {_.map(
+          _.get(this.props, "pageContext.frontmatter.sections", null),
+          (section, section_idx) => {
+            let component = _.upperFirst(
+              _.camelCase(_.get(section, "type", null))
+            );
+            let Component = components[component];
+            return (
+              <Component
+                key={section_idx}
+                {...this.props}
+                section={section}
+                site={this.props.pageContext.site}
+              />
+            );
+          }
+        )}
+      </Layout>
+    );
+  }
 }
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,52 +1,103 @@
-import React from 'react';
-import _ from 'lodash';
-import {graphql} from 'gatsby';
+import React from "react";
+import _ from "lodash";
+import { graphql } from "gatsby";
 
-import {Layout} from '../components/index';
-import {htmlToReact, withPrefix} from '../utils';
-import ScriptInjector from "../services/ScriptInjector";
+import { Layout } from "../components/index";
+import { htmlToReact, withPrefix } from "../utils";
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
 export const query = graphql`
   query($url: String) {
-    sitePage(path: {eq: $url}) {
+    sitePage(path: { eq: $url }) {
       id
     }
   }
 `;
 
+const HUBSPOT_SCRIPT_SRC =
+  "https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js";
+
+const HUBSPOT_IFRAME = `<div class="meetings-iframe-container" data-src="https://meetings.hubspot.com/radimal/getinfo?embed=true"></div>`;
+
 export default class Page extends React.Component {
-    render() {      
+  constructor(props) {
+    super(props);
+    const showHubspotCalendar =
+      typeof window !== `undefined` &&
+      window.location.pathname.replace(/\/+$/, "") === "/signup";
 
-        const scriptInjector = new ScriptInjector()
-        console.log(scriptInjector.getScripts())
+    this.state = {
+      showHubspotCalendar,
+    };
+  }
 
-        return (
-            <Layout {...this.props}>
-            <div className="outer">
-              <div className="inner-medium">
-                <article className="post post-full">
-                  <header className="post-header">
-                    <h1 className="post-title">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
-                    {_.get(this.props, 'pageContext.frontmatter.subtitle', null) && (
-                    <div className="post-subtitle">
-                      {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle', null))}
-                    </div>
+  componentDidMount() {
+    if (!this.state.showHubspotCalendar) return;
+
+    const script = document.createElement("script");
+    script.src = HUBSPOT_SCRIPT_SRC;
+    document.body.appendChild(script);
+    this.setState({ script });
+  }
+
+  componentWillUnmount() {
+    if (!this.state.showHubspotCalendar) return;
+
+    document.body.removeChild(this.state.script);
+  }
+
+  render() {
+    return (
+      <Layout {...this.props}>
+        <div className="outer">
+          <div className="inner-medium">
+            <article className="post post-full">
+              <header className="post-header">
+                <h1 className="post-title">
+                  {_.get(this.props, "pageContext.frontmatter.title", null)}
+                </h1>
+                {_.get(
+                  this.props,
+                  "pageContext.frontmatter.subtitle",
+                  null
+                ) && (
+                  <div className="post-subtitle">
+                    {htmlToReact(
+                      _.get(
+                        this.props,
+                        "pageContext.frontmatter.subtitle",
+                        null
+                      )
                     )}
-                  </header>
-                  {_.get(this.props, 'pageContext.frontmatter.image', null) && (
-                  <div className="post-image">
-                    <img src={withPrefix(_.get(this.props, 'pageContext.frontmatter.image', null))} alt={_.get(this.props, 'pageContext.frontmatter.image_alt', null)} />
                   </div>
-                  )}
-                  <div className="post-content">
-                    {htmlToReact(_.get(this.props, 'pageContext.html', null))}
-                  </div>
-                </article>
+                )}
+              </header>
+              {_.get(this.props, "pageContext.frontmatter.image", null) && (
+                <div className="post-image">
+                  <img
+                    src={withPrefix(
+                      _.get(this.props, "pageContext.frontmatter.image", null)
+                    )}
+                    alt={_.get(
+                      this.props,
+                      "pageContext.frontmatter.image_alt",
+                      null
+                    )}
+                  />
+                </div>
+              )}
+              <div className="post-content">
+                {htmlToReact(_.get(this.props, "pageContext.html", null))}
               </div>
-            </div>
-            </Layout>
-        );
-    }
+
+              {this.state.showHubspotCalendar && (
+                <div dangerouslySetInnerHTML={{ __html: HUBSPOT_IFRAME }} />
+              )}
+            </article>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
