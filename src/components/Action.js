9e56492c986cbb81b8c4e149f1c343f427fcfadf
Stackbit Code Editor <projects@stackbit.com>
2021-10-06 20:29:17 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -14,15 +14,6 @@ export default class Action extends React.Component {
         let action_icon_pos = _.get(action, 'icon_position', null) || 'left';
         return (
             <Link onClick={() => loginWithRedirect()}></Link>
-            <Link to={withPrefix(_.get(action, 'url', null))}
-              {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
-              {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
-              className={classNames({'button': (action_style === 'primary') || (action_style === 'secondary'), 'secondary': action_style === 'secondary', 'has-icon': _.get(action, 'has_icon', null)})}>
-              {_.get(action, 'has_icon', null) && (
-                <Icon {...this.props} icon={action_icon} />
-              )}
-              <span className={classNames({'order-first': action_icon_pos === 'right'})}>{_.get(action, 'label', null)}</span>
-            </Link>
         );
     }
 }
