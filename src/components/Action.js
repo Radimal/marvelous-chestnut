3a72ae01853ba7ec1c06f84a4970e0b5d20b84ba
Stackbit Code Editor <projects@stackbit.com>
2021-10-06 20:27:52 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -11,7 +11,7 @@ export default class Action extends React.Component {
         let action_icon = _.get(action, 'icon', null) || 'arrow-left';
         let action_icon_pos = _.get(action, 'icon_position', null) || 'left';
         return (
-            <Link 
+            <Link onClick=
             <Link to={withPrefix(_.get(action, 'url', null))}
               {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
               {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
